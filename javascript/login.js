6a604e627fe9d60abfc434f430d853c4fc00481c
leandrob0 <leandro.bovino@gmail.com>
2022-05-29 19:52:02 -0300

    files re-structured

--- javascript/login.js
+++ javascript/login.js
@@ -68,7 +68,13 @@ const handleCarousel = (e) => {
 rightChevron.addEventListener('click', handleCarousel)
 leftChevron.addEventListener('click', handleCarousel)
 
-//================================================================
+/*
+############################################
+
+    Code for the form submit functionality.
+
+############################################
+*/
 
 const form = document.getElementById('form');
 
