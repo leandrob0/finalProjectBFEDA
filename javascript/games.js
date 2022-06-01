b7d45bbdb96c2d8792bccb0eb067a05329993ec0
leandrob0 <leandro.bovino@gmail.com>
2022-06-01 19:02:51 -0300

    grid for cards created

--- javascript/games.js
+++ javascript/games.js
@@ -2,14 +2,28 @@
 // If it doesn't exist, it goes back to the login page (it means the user didn't log in.)
 // If it exists, sets the photo from top right to the photo from the user.
 (function () {
-    const userimg = document.querySelector('.user__img');
-    const userinfo = JSON.parse(localStorage.getItem('userinfo'));
+  const userimg = document.querySelector(".user__img");
+  const userinfo = JSON.parse(localStorage.getItem("userinfo"));
 
-    if(!userinfo) {
-        location.href = '/';
-    }
+  if (!userinfo) {
+    location.href = "/";
+  }
+
+  userimg.src = `../resources/images/${userinfo.user.photo}`;
+})();
+
+// Cards simulation to test the cards visuals.
+const cards = [
+  { title: "Biomutant", release: "25-01-2021", genres: ["Action", "RPG"] },
+  { title: "Biomutant", release: "25-01-2021", genres: ["Action", "RPG"] },
+  { title: "Biomutant", release: "25-01-2021", genres: ["Action", "RPG"] },
+  { title: "Biomutant", release: "25-01-2021", genres: ["Action", "RPG"] },
+  { title: "Biomutant", release: "25-01-2021", genres: ["Action", "RPG"] },
+  { title: "Biomutant", release: "25-01-2021", genres: ["Action", "RPG"] },
+];
+
+(function () {
     
-    userimg.src = `../resources/images/${userinfo.user.photo}`;
 })();
 
 /*
@@ -20,8 +34,8 @@
 ############################################
 */
 
-const logoutButton = document.querySelector('.user__log-out');
+const logoutButton = document.querySelector(".user__log-out");
 
-logoutButton.addEventListener('click', () => {
-    localStorage.removeItem('userinfo');
-})
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("userinfo");
+});
