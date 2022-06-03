6ab962a05fd6d511a2fa5756619c24ad976a6eaa
Leandro <75281214+leandrob0@users.noreply.github.com>
2022-06-03 19:00:25 -0300

    Login light mode. (#7)
    * images added, refactored html, light photos and background of the login showing.
    
    * chevrons and bottom slider light mode added
    
    * localStorage bug working fixed. added post method to form to not add login credentials to url
    
    * background for the logo added
    
    * login light mode done

--- javascript/login.js
+++ javascript/login.js
@@ -1,11 +1,36 @@
+// Elements used in this script.
+const carousel = document.getElementById("bg-carousel");
+const rightChevron = document.getElementById("right-chevron");
+const leftChevron = document.getElementById("left-chevron");
+const bottomSliderChildren = document.getElementById("bottom-slider").children;
+
+const headerLightMode = document.querySelector('.not-show');
+const logo = document.querySelector("#logo-login");
+const body = document.querySelector("body");
+const loginContainer = document.querySelector("main");
+
+const form = document.getElementById("form");
+const btnSocialsIcons = document.querySelectorAll('.btn-icon');
+
+// This array contains all the names of the images for the background.
+const images = [
+  { dark: "BG.png", light: "default-xbox-L.png" },
+  { dark: "Headphones-D.jpg", light: "graphic-card-L.jpg" },
+  { dark: "Keyboard-D.jpg", light: "keyboard-L.jpg" },
+  { dark: "PsController-D.jpg", light: "ps5-controller-L.jpg" },
+  { dark: "VideoCard-D.jpg", light: "Ps5-L.jpg" },
+  { dark: "XboxController-D.jpg", light: "Xbox-Pink-L.jpg" },
+];
+
 // If the user is logged  in, it doesn't allow him to be in the login page.
 (function () {
-  const userinfo = JSON.parse(localStorage.getItem('userinfo'));
+  const userinfo = JSON.parse(localStorage.getItem("userinfo"));
 
-    if(userinfo) {
-        location.href = '/games.html';
-    }
+  if (userinfo) {
+    location.href = "/games.html";
+  }
 })();
+
 
 
 /*
@@ -39,32 +64,35 @@ eye.addEventListener("click", () => {
 ############################################
 */
 
-const carousel = document.getElementById("bg-carousel");
-const rightChevron = document.getElementById("right-chevron");
-const leftChevron = document.getElementById("left-chevron");
-const bottomSliderChildren = document.getElementById("bottom-slider").children;
-
-// This array contains all the names of the dark mode images.
-const imagesDm = [
-  "BG.png",
-  "Headphones-D.jpg",
-  "Keyboard-D.jpg",
-  "PsController-D.jpg",
-  "VideoCard-D.jpg",
-  "XboxController-D.jpg",
-];
-
 // Used an anonymous function so it is ran when it gets here, setting the previous image the user had.
 (function () {
   let actualPhoto = JSON.parse(localStorage.getItem("carousel")) || {
     index: 0,
     mode: "dark",
   };
+  const mode = actualPhoto.mode;
 
+  // Sets the current background photo depending on the color mode.
   carousel.style.backgroundImage = `url('../resources/images/${
-    imagesDm[actualPhoto.index]
+    mode === "dark"
+      ? images[actualPhoto.index].dark
+      : images[actualPhoto.index].light
   }')`;
+
   bottomSliderChildren[actualPhoto.index + 1].style.opacity = "1";
+
+  // Handles the case in where the user selected light mode, so it is saved in localStorage.
+  if (mode === "light") {
+    logo.src = "./resources/logos/Black.svg";
+    body.classList.add("light-mode");
+    loginContainer.style.backgroundColor = '#F0F0F0';
+    headerLightMode.setAttribute('id', 'show-header');
+    btnSocialsIcons.forEach((icon) => {
+      icon.classList.add('light-btn-icon');
+    })
+  } 
+
+  localStorage.setItem("carousel", JSON.stringify(actualPhoto));
 })();
 
 const handleCarousel = (e) => {
@@ -94,11 +122,12 @@ const handleCarousel = (e) => {
     bottomSliderChildren[actualPhoto.index + 1].style.opacity = "1";
   }
 
-  if (actualPhoto.mode === "dark") {
-    carousel.style.backgroundImage = `url('../resources/images/${
-      imagesDm[actualPhoto.index]
-    }')`;
-  }
+  carousel.style.backgroundImage = `url('../resources/images/${
+    actualPhoto.mode === "dark"
+      ? images[actualPhoto.index].dark
+      : images[actualPhoto.index].light
+  }')`;
+
   localStorage.setItem("carousel", JSON.stringify(actualPhoto));
 };
 
@@ -127,7 +156,6 @@ const loginUser = (email, password) => {
   });
 };
 
-const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -142,11 +170,11 @@ form.addEventListener("submit", (e) => {
       return res.json();
     })
     .then((data) => {
-      if(typeof data === 'object') {
-        localStorage.setItem('userinfo', JSON.stringify(data));
-        location.href = 'games.html';
+      if (typeof data === "object") {
+        localStorage.setItem("userinfo", JSON.stringify(data));
+        location.href = "games.html";
       } else {
-          throw new Error();
+        throw new Error();
       }
     })
     .catch(() => {
